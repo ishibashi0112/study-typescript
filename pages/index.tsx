12f42e71c99350e3b76e5d6ae4074fde79a0e9ca
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-08 03:58:07 +0900

    🚧 勉強中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import type { NextPage } from 'next'
 
+let foo:number =123
+function double(x:number):number | undefined{
+  if(x<0){
+    return
+  }
+  return x * 2
+}
+
+let hoge = {} as {bar:number}
+hoge.bar = 1
+
+
+const boo: boolean = false
+const str: string = "test"
+const num: number = 123
+const nu: null = null
+const und: undefined = undefined
+
+
 const Home: NextPage = () => {
-  return (
-    
-    <div>test</div>
-  )
+  return (<div>test</div>)
 }
 
 export default Home
